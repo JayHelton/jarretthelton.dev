1856a5b4c6748f028303587822589661fa0ec1ea
Jarrett Helton <jaydhelton@gmail.com>
2021-08-07 16:48:40 -0400

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  darkMode: 'class',
   purge: {
     mode: "all",
     content: ["./**/*.html"],
@@ -10,10 +11,38 @@ module.exports = {
     container: {
       center: true,
     },
-    extend: {
-      colors: {},
+     extend: {
+      typography(theme) {
+        return {
+          DEFAULT: {
+            css: {
+              color: theme("colors.gray.300"),
+              '[class~="lead"]': { color: theme("colors.gray.400") },
+              a: { color: theme("colors.gray.100") },
+              strong: { color: theme("colors.gray.100") },
+              "ul > li::before": { backgroundColor: theme("colors.pink.700") },
+              "ol > li::before": { color: theme("colors.pink.700") },
+              hr: { borderColor: theme("colors.pink.700") },
+              blockquote: {
+                color: theme("colors.gray.100"),
+                borderLeftColor: theme("colors.pink.700"),
+              },
+              h1: { color: theme("colors.gray.100") },
+              h2: { color: theme("colors.gray.100") },
+              h3: { color: theme("colors.gray.100") },
+              h4: { color: theme("colors.gray.100") },
+              thead: {
+                color: theme("colors.gray.100"),
+                borderBottomColor: theme("colors.gray.700"),
+              },
+              "tbody tr": { borderBottomColor: theme("colors.gray.800") },
+            },
+          },
+        };
+      },
     },
   },
-  variants: {},
+  variants: {
+  },
   plugins: [require("@tailwindcss/typography")],
 };
